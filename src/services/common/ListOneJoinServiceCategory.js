1d7e297d7ceecb08e06dac6e54e23bc0118c3317
mhtanim <mhtanim2@gmail.com>
2023-08-28 00:21:12 +0600

    added

---
+++ src/services/common/ListOneJoinServiceCategory.js
@@ -0,0 +1,67 @@
+const ListOneJoinServiceCategory = async (Request, DataModel, SearchArray, JoinStage) => {
+  try {
+    const searchValue = Request.params.searchKeyword;
+    let data;
+
+    (searchValue !== '0') ? data = await DataModel.aggregate([
+      JoinStage,
+      {
+        $unwind: '$category',
+      },
+      { $match: { $or: SearchArray } },
+      {
+        $group: {
+          _id: '$category._id',
+          ItemCategory: { $first: '$category.ItemCategory' },
+          Items: {
+            $push: {
+              _id: '$_id',
+              ItemName: '$ItemName',
+              Description: '$Description',
+              UnitPrice: '$UnitPrice',
+              Discount: '$Discount',
+              ItemImage: '$ItemImage',
+              CreatedDate: '$CreatedDate',
+              RoomCount: '$RoomCount',
+              BathRoomCont: '$BathRoomCont',
+              GuestCount: '$GutterCount',
+              LocationValue: '$LocationValue',
+            },
+          },
+        },
+      },
+    ]) : data = await DataModel.aggregate([
+      JoinStage,
+      {
+        $unwind: '$category',
+      },
+      {
+        $group: {
+          _id: '$category._id',
+          ItemCategory: { $first: '$category.ItemCategory' },
+          Items: {
+            $push: {
+              _id: '$_id',
+              ItemName: '$ItemName',
+              Description: '$Description',
+              UnitPrice: '$UnitPrice',
+              Discount: '$Discount',
+              ItemImage: '$ItemImage',
+              CreatedDate: '$CreatedDate',
+              RoomCount: '$RoomCount',
+              BathRoomCont: '$BathRoomCont',
+              GuestCount: '$GutterCount',
+              LocationValue: '$LocationValue',
+            },
+          },
+        },
+      },
+    ]);
+
+    return { status: 'success', data };
+  } catch (error) {
+    return { status: 'fail', data: error };
+  }
+};
+
+module.exports = ListOneJoinServiceCategory;
